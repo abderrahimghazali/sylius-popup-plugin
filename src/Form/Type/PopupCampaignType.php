02a504f1770a1d3ed3f85b15816ba376b1857951
Abderrahim GHAZALI <ghazali.abderrahim1@gmail.com>
2026-03-27 23:44:57 +0100

    Fix: use AbstractType instead of AbstractResourceType for form
    AbstractResourceType requires $dataClass constructor injection which
    conflicts with manual controller usage. Switch to standard AbstractType
    with configureOptions data_class instead.

--- src/Form/Type/PopupCampaignType.php
+++ src/Form/Type/PopupCampaignType.php
@@ -9,7 +9,8 @@ use Abderrahim\SyliusPopupPlugin\Enum\ShowFrequency;
 use Abderrahim\SyliusPopupPlugin\Enum\TargetAudience;
 use Abderrahim\SyliusPopupPlugin\Enum\TargetPages;
 use Abderrahim\SyliusPopupPlugin\Enum\TriggerType;
-use Sylius\Bundle\ResourceBundle\Form\Type\AbstractResourceType;
+use Abderrahim\SyliusPopupPlugin\Entity\PopupCampaign;
+use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\ColorType;
 use Symfony\Component\Form\Extension\Core\Type\EnumType;
@@ -18,12 +19,13 @@ use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Validator\Constraints\PositiveOrZero;
 use Symfony\Component\Validator\Constraints\Range;
 
-final class PopupCampaignType extends AbstractResourceType
+final class PopupCampaignType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
@@ -128,6 +130,13 @@ final class PopupCampaignType extends AbstractResourceType
                 ],
             ])
         ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => PopupCampaign::class,
+        ]);
     }
 
     public function getBlockPrefix(): string
